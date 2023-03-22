51b3e0174c3566e515d8d0db1e5a33636715595f
Dmitriy Ovchinnikov <dlcerberos@gmail.com>
2023-03-23 02:48:29 +0300

    index sceletons

---
+++ my-app/src/components/Pages/PageIndex/PageIndexComponents/IndexSeazon/IndexSeazonSceleton.tsx
@@ -0,0 +1,72 @@
+import { FlexWrapper } from "../../../../StyledComponents/FlexWrapper/FlexWrapper";
+import { Span } from "../../../../StyledComponents/Span/Span";
+
+export const IndexSceletonSeazon = () => {
+  return (
+    <>
+      <FlexWrapper gap="30px">
+        <FlexWrapper
+          height="260px"
+          width="140px"
+          backgroundCcolor="#e9e9e9"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="space-between"
+          borderRadius="5px"
+          padding="20px 10px"
+          position="relative"
+        >
+          <FlexWrapper position="absolute" top="5px" right="5px">
+            <FlexWrapper
+              height="28px"
+              width="28px"
+              borderRadius="50%"
+              backgroundCcolor="#b2b2b2"
+            />
+          </FlexWrapper>
+
+          <FlexWrapper
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            gap="10px"
+          >
+            <FlexWrapper
+              width="45px"
+              height="60px"
+              backgroundCcolor="#b2b2b2"
+              borderRadius="5px"
+            />
+            <FlexWrapper
+              width="102px"
+              height="18px"
+              backgroundCcolor="#b2b2b2"
+              borderRadius="5px"
+            />
+            <FlexWrapper
+              width="102px"
+              height="18px"
+              backgroundCcolor="#b2b2b2"
+              borderRadius="5px"
+            />
+            <FlexWrapper
+              width="102px"
+              height="18px"
+              backgroundCcolor="#b2b2b2"
+              borderRadius="5px"
+            />
+          </FlexWrapper>
+
+          <FlexWrapper />
+
+          <FlexWrapper
+            width="102px"
+            height="30px"
+            backgroundCcolor="#b2b2b2"
+            borderRadius="5px"
+          />
+        </FlexWrapper>
+      </FlexWrapper>
+    </>
+  );
+};
